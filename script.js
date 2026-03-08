a48984485d0fdea60130c21491426a3eae86e9c3
arsicko <realarss@gmail.com>
2026-03-08 17:03:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-// script.js
 const form = document.getElementById('registrationForm');
 
 form.addEventListener('submit', e => {
-  e.preventDefault(); // Останавливаем стандартную отправку
+  e.preventDefault();
 
-  const organization = form.organization.value;
-  const name = form.name.value;
-  const position = form.position.value;
+  const formData = new FormData(form);
+
+  const data = {};
+  formData.forEach((value, key) => {
+    data[key] = value;
+  });
 
   const scriptURL = 'https://script.google.com/macros/s/AKfycbwRQVYJJx6aMcPKU423kYheZusSPqFRPWoP-I_8ttb8DHAjeJa-ORv8pVQeKY0iOg7S/exec';
 
   fetch(scriptURL, {
     method: 'POST',
-    body: JSON.stringify({ organization, name, position }),
-    headers: { 'Content-Type': 'application/json' }
+    body: new URLSearchParams(data) // отправляем как form-urlencoded
   })
-  .then(response => {
-    // Переход на страницу Спасибо
-    window.location.href = 'thanks.html';
-  })
+  .then(response => window.location.href = 'thanks.html')
   .catch(error => {
     alert('Ошибка отправки, попробуйте снова');
     console.error(error);
   });
-
 });
+
