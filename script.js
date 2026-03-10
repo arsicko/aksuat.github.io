01cb5f263eae62a96654163fafa1b3165c29d466
arsicko <realarss@gmail.com>
2026-03-10 12:00:27 +0500

    Refactor form submission event listener
    Refactor form submission handling to use function syntax and simplify data handling.

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 const form = document.getElementById('registrationForm');
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', function(e) {
+
   e.preventDefault();
 
   const formData = new FormData(form);
 
-  const data = {};
-  formData.forEach((value, key) => {
-    data[key] = value;
+  fetch("https://script.google.com/macros/s/AKfycbycJUTWPBMV7O1BbFpRnrboJWaIOR6-U9b9qWYrUD54ccBqniXhntrIGOv0e8SybUx7/exec", {
+    method: "POST",
+    body: formData
   });
 
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbycJUTWPBMV7O1BbFpRnrboJWaIOR6-U9b9qWYrUD54ccBqniXhntrIGOv0e8SybUx7/exec';
-
-  fetch(scriptURL, {
-    method: 'POST',
-    body: new URLSearchParams(data) // отправляем как form-urlencoded
-  })
-  .then(response => window.location.href = 'thanks.html')
-  .catch(error => {
-    alert('Ошибка отправки, попробуйте снова');
-    console.error(error);
-  });
+  window.location.href = "thanks.html";
 
 });
 
